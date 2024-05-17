142c560a6a880433dd97d6a59095ca1b2fba05c3
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-17 15:16:25 +0330

    add channels and daphne

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # daphne
+    'daphne',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     # my apps
     'chat',
     'accounts',
+
 ]
 
 MIDDLEWARE = [
