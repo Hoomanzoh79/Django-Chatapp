e54a31a1184121414cb80d013551e988bcc7fcb2
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-18 17:25:25 +0330

    add templates path

--- core/settings.py
+++ core/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath("templates"))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
