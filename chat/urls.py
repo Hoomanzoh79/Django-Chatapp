e4b2692b2a01e6cb19a1f101a00a948ab127ba59
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-18 17:27:45 +0330

    add room

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
+    path("<str:room_name>/", views.room, name="room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "chat/index.html"
+
+
+def room(request, room_name):
+    return render(request, "chat/room.html", {"room_name": room_name})
