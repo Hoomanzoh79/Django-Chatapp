dd940323917a42afd1e799625a3846b270a13cff
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-19 16:13:25 +0330

    add app_name

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'chat'
+
 urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
     path("<str:room_name>/", views.room_view, name="room"),
