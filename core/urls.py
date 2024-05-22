fcd9c50702216ca8827c77696b5907db36ee292a
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-22 14:58:34 +0330

    add basic authentication

--- core/urls.py
+++ core/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('chat/',include('chat.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
