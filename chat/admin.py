4c621d9a74436b156ff8ea2148ef9fafc69667c0
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-24 13:42:21 +0330

    register Room and Message models

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Room,Message
 
-# Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    model = Room
+    list_display = ["name"]
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_display = ["room","content","author"]
