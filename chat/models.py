64b292521f9adf0a360c9dd44b44013c4e74e9cf
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-24 13:42:08 +0330

    add Room and Message Models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=30,)
+    online = models.ManyToManyField(get_user_model(),blank=True)
+
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def __str__(self):
+        return f'{self.name} : ({self.get_online_count()})'
+
+
+class Message(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username}: {self.content}'
