d5a74a105b8afafd6aff4e29b3db22b257e00c2d
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-24 13:55:41 +0330

    add rooms as context to IndexView

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import Room
 
 class IndexView(TemplateView):
     template_name = "chat/index.html"
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context["rooms"] = Room.objects.all()
+        return context
 
 
 def room_view(request, room_name):
