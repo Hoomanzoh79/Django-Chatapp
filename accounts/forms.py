79e79f31bcc801546d55172293213977e7456a87
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-31 11:06:33 +0330

    change signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,4 +5,4 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ("username",)
+        fields = ("username","email","first_name","last_name",)
