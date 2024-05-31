763887d2e46dc7fee6315946479a998a369c1ff8
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-31 11:59:02 +0330

    add phone_number and bio to fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
     is_superuser = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
+
+    phone_number = models.CharField(max_length=12, 
+                                    validators=[RegexValidator(regex="^[0][9][0-9][0-9]{8,8}$")],)
+    bio = models.CharField(max_length=350,blank=True)
+    
