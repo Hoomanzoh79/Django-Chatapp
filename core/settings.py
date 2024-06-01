a051a20e02081e140c4e73b31087eac46b44130e
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-01 12:53:34 +0330

    add decouple for env vars

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dzkcgbt)#q_5r3@2!hji(02*@selx6%_m5$p&98*_p-n=8^jgu'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG",cast=bool,default=True)
 
 ALLOWED_HOSTS = []
 
