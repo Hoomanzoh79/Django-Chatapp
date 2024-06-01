b8e35e12c515e27f8842b90334b36fad95c16861
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-01 11:13:15 +0330

    clean up form text data

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("username","email","first_name","last_name","phone_number",)
+    
+    # clean up form text data
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
 
 
 class CustomUserChangeForm(UserChangeForm):
