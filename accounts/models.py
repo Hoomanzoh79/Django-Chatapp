aa995e268908c6b6d6da055a2dd1a6a21b36fc85
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-01 11:12:54 +0330

    add comment

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,8 @@ class CustomUser(AbstractUser):
     is_superuser = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
 
+    # make unique later 
     phone_number = models.CharField(max_length=12, 
-                                    validators=[RegexValidator(regex="^[0][9][0-9][0-9]{8,8}$")],)
+                                    validators=[RegexValidator(regex="^[0][9][0-9][0-9]{8,8}$")])
     bio = models.CharField(max_length=350,blank=True)
     
