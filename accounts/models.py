826c2627d7b403a9a75439fc854455377ba83dc0
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-01 11:19:51 +0330

    change email field for CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,7 @@ class CustomUser(AbstractUser):
     is_verified = models.BooleanField(default=False)
 
     # make unique later 
+    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=12, 
                                     validators=[RegexValidator(regex="^[0][9][0-9][0-9]{8,8}$")])
     bio = models.CharField(max_length=350,blank=True)
