dab394747d1411ffdd9b6c0b939b6161e8e44102
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-02 10:54:17 +0330

    add Follow model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Follow(models.Model):
+    follower = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name="follower")
+
+    followed = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name="followed")
+
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
+
