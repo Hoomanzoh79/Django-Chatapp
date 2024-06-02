fa1828a4470d434798f3a1ff6ef6c57765ba1c5a
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-02 11:30:57 +0330

    add SearchResultsView

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/',include('accounts.urls')),
+    path('social/',include('social.urls')),
     path('',include('pages.urls')),
 ]
--- social/views.py
+++ social/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+class SearchResultsView(ListView):
+    model = get_user_model()
+    template_name = "social/search_results.html"
+    context_object_name = 'users'
+
+    def get_queryset(self):  # new
+        search_query = self.request.GET.get("q")
+        if not search_query:
+            search_query = ""
+        object_list = self.model.objects.filter(
+            username__icontains=search_query
+        )
+        return object_list
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchResultsView, self).get_context_data(**kwargs)
+        context.update({
+            'search_query': self.request.GET.get("q"),
+        })
+        return context
