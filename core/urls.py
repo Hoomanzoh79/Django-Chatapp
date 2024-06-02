dc26b3f6c66592b747f47bb2b1f804d188b895f6
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-02 11:14:21 +0330

    add home page

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/',include('accounts.urls')),
+    path('',include('pages.urls')),
 ]
---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('',views.HomePageView.as_view(),name="home"),
+]
