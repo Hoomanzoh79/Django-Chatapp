f1aff385e61717ae0c48ab55055893cac3d25c15
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-05 10:21:32 +0330

    add FollowView

--- social/urls.py
+++ social/urls.py
@@ -6,4 +6,5 @@ app_name = 'social'
 
 urlpatterns = [
     path('',views.SearchResultsView.as_view(),name="search"),
+    path('follow/<str:username>/',views.FollowView.as_view(),name="follow"),
 ]
--- social/views.py
+++ social/views.py
@@ -1,10 +1,13 @@
-from django.views.generic import ListView
-from django.contrib.auth import get_user_model
+from django.views import generic
+from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import get_object_or_404,redirect
+from django.urls import reverse
+from django.contrib import messages
 
 from accounts.models import CustomUser
+from .models import Follow
 
-
-class SearchResultsView(ListView):
+class SearchResultsView(generic.ListView):
     model = CustomUser
     template_name = "social/search_results.html"
     context_object_name = 'users'
@@ -24,3 +27,18 @@ class SearchResultsView(ListView):
             'search_query': self.request.GET.get("q"),
         })
         return context
+
+
+class FollowView(generic.View):
+    def get(self,request,username):
+        follower = self.request.user
+        followed = get_object_or_404(CustomUser,username=username)
+        if Follow.objects.filter(follower=follower,followed=followed).exists():
+             messages.warning(self.request,"You already followed this user !")
+        else:
+            Follow.objects.create(follower=follower,followed=followed)
+            messages.success(self.request,"You successfully followed this user")
+        return redirect("pages:home")
+    
+    def post(self,request):
+        return HttpResponse("post")
