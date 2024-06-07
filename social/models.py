96a9ec1218ed8a226abebf7a39a72fc48bcc0112
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-07 12:22:37 +0330

    change followed to following

--- social/models.py
+++ social/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth import get_user_model
 class Follow(models.Model):
     follower = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name="follower")
 
-    following = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name="following",null=True)
+    following = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name="following")
 
     follow_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.follower} follows {self.followed}"
+        return f"{self.follower} follows {self.following}"
 
