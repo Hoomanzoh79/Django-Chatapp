ec73716911546ac1e3df3ab9a6551eb873c3ac61
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-08 10:01:25 +0330

    add follow stats and profile stats as context

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
-from django.db.models.query import QuerySet
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
 from .forms import CustomUserCreationForm,CustomUserChangeForm
 from .models import CustomUser
+from social.models import Follow
 
 
 class SignUpView(generic.CreateView):
@@ -31,3 +32,22 @@ class AccountDetailView(generic.DetailView):
     template_name = "accounts/account_detail.html"
     context_object_name = "account"
     slug_field = "username"
+
+    def get_context_data(self, **kwargs):
+        context = super(AccountDetailView, self).get_context_data(**kwargs)
+        follower_user = self.request.user
+        username = self.kwargs['slug']
+        following_user = get_object_or_404(get_user_model(),username=username)
+        # follow status
+        follow_status = Follow.objects.filter(following=following_user,follower=follower_user).exists()
+        
+        # profile stats
+        followings_count = Follow.objects.filter(follower=following_user).count()
+        followers_count = Follow.objects.filter(following=following_user).count()
+
+        context.update({
+            'follow_status':follow_status,
+            'followings':followings_count,
+            'followers':followers_count,
+        })
+        return context
