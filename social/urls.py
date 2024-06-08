44939f7db97d4e349ef34e575bfdbe70611ed560
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-08 10:01:52 +0330

    add follow

--- social/urls.py
+++ social/urls.py
@@ -6,5 +6,5 @@ app_name = 'social'
 
 urlpatterns = [
     path('',views.SearchResultsView.as_view(),name="search"),
-    path('follow/<str:username>/',views.FollowView.as_view(),name="follow"),
+    path('follow/<str:username>/<option>',views.follow,name="follow"),
 ]
--- social/views.py
+++ social/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponseRedirect,HttpResponse
 from django.shortcuts import get_object_or_404,redirect
 from django.urls import reverse
 from django.contrib import messages
+from django.contrib.auth import get_user_model
 
 from accounts.models import CustomUser
 from .models import Follow
 
 class SearchResultsView(generic.ListView):
-    model = CustomUser
+    model = get_user_model()
     template_name = "social/search_results.html"
     context_object_name = 'users'
 
@@ -23,22 +24,29 @@ class SearchResultsView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super(SearchResultsView, self).get_context_data(**kwargs)
+        search_query = self.request.GET.get("q")
+        following = self.model.objects.filter(
+            username__icontains=search_query
+        )
+        for user in following.all():
+            follower = self.request.user
+            # Follow status
+            follow_status = Follow.objects.filter(following=user,follower=follower).exists()
+
         context.update({
             'search_query': self.request.GET.get("q"),
+            'follow_status':follow_status,
         })
         return context
 
+def follow(request,username,option):
+    user = request.user
+    following = get_object_or_404(get_user_model(),username=username)
+    try:
+        f, created = Follow.objects.get_or_create(follower=user,following=following)
 
-class FollowView(generic.View):
-    def get(self,request,username):
-        follower = self.request.user
-        followed = get_object_or_404(CustomUser,username=username)
-        if Follow.objects.filter(follower=follower,followed=followed).exists():
-             messages.warning(self.request,"You already followed this user !")
-        else:
-            Follow.objects.create(follower=follower,followed=followed)
-            messages.success(self.request,"You successfully followed this user")
-        return redirect("pages:home")
-    
-    def post(self,request):
-        return HttpResponse("post")
+        if int(option) == 0 :
+            f.delete()
+        return HttpResponseRedirect(reverse("accounts:account-detail",kwargs={"slug":username}))
+    except CustomUser.DoesNotExist:
+        return HttpResponseRedirect(reverse("accounts:account-detail",kwargs={"slug":username}))
