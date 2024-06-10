cc333347b2ee8d5a43ec317c4289ee34aed7e711
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-10 10:56:09 +0330

    add profile_picture

--- accounts/models.py
+++ accounts/models.py
@@ -13,7 +13,7 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=12, 
                                     validators=[RegexValidator(regex="^[0][9][0-9][0-9]{8,8}$")])
     bio = models.CharField(max_length=350,blank=True)
-    # profile_picture = models.ImageField()
+    profile_picture = models.ImageField(upload_to='accounts/profile_pictures', blank=True)
     
     def get_absolute_url(self):
         return reverse("accounts:account-detail", kwargs={"slug": self.username})
