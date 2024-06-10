51116bdebc06d479685a40564072c3405f3165c8
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-10 10:41:25 +0330

    add media settings

--- core/settings.py
+++ core/settings.py
@@ -133,6 +133,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# media files
+MEDIA_ROOT = str(BASE_DIR.joinpath('media/'))
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
