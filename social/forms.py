7a91ea0587831c4d5cc2103e782929eb48bc9cd7
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-12 12:25:31 +0330

    add CommentForm

--- social/forms.py
+++ social/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
 
-from .models import Post
+from .models import Post,Comment
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ["picture","caption"]
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body','parent']
