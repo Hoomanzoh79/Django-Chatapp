386ffb91a8e2423cda3ce133eae5f05c014c5770
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-13 10:52:13 +0330

    change static file dirs

---
+++ static/js/room.js
@@ -0,0 +1,42 @@
+// chat/static/room.js
+
+console.log("Sanity check from room.js.");
+
+const roomName = JSON.parse(document.getElementById('roomName').textContent);
+
+let chatLog = document.querySelector("#chatLog");
+let chatMessageInput = document.querySelector("#chatMessageInput");
+let chatMessageSend = document.querySelector("#chatMessageSend");
+let onlineUsersSelector = document.querySelector("#onlineUsersSelector");
+
+// adds a new option to 'onlineUsersSelector'
+function onlineUsersSelectorAdd(value) {
+    if (document.querySelector("option[value='" + value + "']")) return;
+    let newOption = document.createElement("option");
+    newOption.value = value;
+    newOption.innerHTML = value;
+    onlineUsersSelector.appendChild(newOption);
+}
+
+// removes an option from 'onlineUsersSelector'
+function onlineUsersSelectorRemove(value) {
+    let oldOption = document.querySelector("option[value='" + value + "']");
+    if (oldOption !== null) oldOption.remove();
+}
+
+// focus 'chatMessageInput' when user opens the page
+chatMessageInput.focus();
+
+// submit if the user presses the enter key
+chatMessageInput.onkeyup = function(e) {
+    if (e.keyCode === 13) {  // enter key
+        chatMessageSend.click();
+    }
+};
+
+// clear the 'chatMessageInput' and forward the message
+chatMessageSend.onclick = function() {
+    if (chatMessageInput.value.length === 0) return;
+    // TODO: forward the message to the WebSocket
+    chatMessageInput.value = "";
+};
