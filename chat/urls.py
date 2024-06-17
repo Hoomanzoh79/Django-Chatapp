8fc80d8714fdb6dd296aefe36974b6b3122b6442
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-06-17 12:38:20 +0330

    add message

--- chat/urls.py
+++ chat/urls.py
@@ -6,5 +6,6 @@ app_name = 'chat'
 
 urlpatterns = [
     path('',views.ChatIndexView.as_view(),name="chat-index"),
-    path("<str:room_name>/", views.room_view, name="room"),
+    path('<str:user1>/<str:user2>/',views.message,name="message"),
+    # path("<str:room_name>/", views.room_view, name="room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponseBadRequest
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 from .models import Room
 
@@ -13,8 +18,45 @@ class ChatIndexView(TemplateView):
         return context
 
 
-def room_view(request, room_name):
-    chat_room, created = Room.objects.get_or_create(name=room_name)
+def message(request,user1,user2):
+    user1_obj = get_object_or_404(get_user_model(),username=user1)
+    user2_obj = get_object_or_404(get_user_model(),username=user2)
+
+    if Room.objects.filter(Q(name__icontains=user1)& Q(name__icontains=user2)).exists():
+        room = Room.objects.get(Q(name__icontains=user1)& Q(name__icontains=user2))
+        if request.user != room.user1 and request.user != room.user2:
+            return HttpResponseBadRequest("You cant't join this private chat!")
+
+    else:
+        room_name = f"{user1}and{user2}"
+        room = Room.objects.create(name=room_name)
+        
+        room.user1 = user1_obj
+        room.user2 = user2_obj
+        room.save()
+
     return render(request, 'chat/room.html', {
-        'room': chat_room,
+        'room': room,
+        'target_user':user2_obj,
     })
+
+
+# def room_view(request, room_name):
+#     room, created = Room.objects.get_or_create(name=room_name)
+
+#     if room.user1 and room.user2:
+#         return HttpResponseBadRequest("You cant't join this private chat!")
+    
+#     if not room.user1 :
+#         room.user1 = request.user
+
+#     elif not room.user2 :
+#         room.user2 = request.user
+        
+#     room.save()
+
+#     if request.user != room.user1 and request.user != room.user2:
+#             return HttpResponseBadRequest("You cant't join this private chat!")
+#     return render(request, 'chat/room.html', {
+#         'room': room,
+#     })
